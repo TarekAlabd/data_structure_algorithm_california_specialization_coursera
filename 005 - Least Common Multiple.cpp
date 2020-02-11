e6b06016f07e3707e16cbe41050ab11ff8ef3da4
Tarek Alabd <tarekalabd1@gmail.com>
2020-02-11 17:51:42 +0200

    Create 005 - Least Common Multiple

---
+++ 005 - Least Common Multiple.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long) a * b;
+}
+
+long long gcf(int a, int b) {
+    if (b==0)
+        return a;
+    else
+        return gcf(b, a%b);
+}
+
+long long lcm_fast(int a, int b) {
+    return ((long long) a*b)/gcf(a,b);
+}
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << lcm_fast(a, b) << std::endl;
+  return 0;
+}
